f696b39ad554fc89e98b73483184091ca3461ba5
Bunmi Oye <bunmioye09@gmail.com>
2022-11-14 19:18:11 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import Form from './components/Form';
 import LinkedInAuth from './components/LinkedInAuth';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [linkedInAuth, setLinkedInAuth] = useState(false);
 
   return (
-    <Box
-    // TODO delete bg
-    // bg="white"
-    >
+    <Box>
       <Navbar />
-      <Title />
-      <LinkedInAuth setLinkedInAuth={setLinkedInAuth} />
-      <Form linkedInAuth={linkedInAuth} />
+
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Title />
+                <LinkedInAuth setLinkedInAuth={setLinkedInAuth} />
+                <Form linkedInAuth={linkedInAuth} />
+              </>
+            }
+          />
+          <Route
+            path="/volunteer-agreement"
+            element={<h1>Volunteer form</h1>}
+          />
+        </Routes>
+      </Router>
+
       <Footer />
     </Box>
   );
